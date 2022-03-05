8cd127bc92a6b27aead36565b622fae9d9a6e87d
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-03-05 17:47:28 +0900

    createアクション追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,11 +9,16 @@ class ItemsController < ApplicationController
   end
 
   def create
-    
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
-  def message_params
+  def item_params
     params.require(:item).permit(:name, :image, :info, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :schedule_id, :price).merge(user_id: current_user.id)
   end
 end
