41df75003682431c91c4c916e37117f0feb6e6ef
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-03-05 14:21:08 +0900

    モデルテスト項目洗い出し

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,8 +4,8 @@ FactoryBot.define do
     email                 {Faker::Internet.free_email}
     password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
-    first_name            {'田中'}
-    last_name             {'リチャード'}
+    first_name            {"田中"}
+    last_name             {"リチャード"}
     first_name_kana       {"タナカ"}
     last_name_kana        {"リチャード"}}
     birth_date            {Faker::Date.birthday(min_age: 18, max_age: 65)}
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,82 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録できる場合' do
+      it "全ての項目が正しければ登録できる" do
+
+      end
+    end
+
+    context '新規登録できない場合' do
+      it "nicknameが空では登録できない" do
+
+      end
+      it "emailが空では登録できない" do
+
+      end
+      it "passwordが空では登録できない" do
+
+      end
+      it "first_nameが空では登録できない" do
+
+      end
+      it "last_nameが空では登録できない" do
+
+      end
+      it "first_name_kanaが空では登録できない" do
+
+      end
+      it "last_name_kanaが空では登録できない" do
+
+      end
+      it "birth_dateが空では登録できない" do
+
+      end
+      #ここまで空テスト
+      #ここから文字種類、文字数テスト
+      it "passwordが数字だけでは登録できない" do
+
+      end
+      it "passwordが英字では登録できない" do
+
+      end
+      it "passwordが6文字以内では登録できない" do
+
+      end
+      it "passwordが129文字以上では登録できない" do
+
+      end
+      it "first_nameが英字では登録できない" do
+
+      end
+      it "last_nameが英字では登録できない" do
+
+      end
+      it "first_name_kanaが漢字では登録できない" do
+
+      end
+      it "last_name_kanaが漢字では登録できない" do
+
+      end
+      it "first_name_kanaがひらがなでは登録できない" do
+
+      end
+      it "last_name_kanaがひらがなでは登録できない" do
+
+      end
+      it "first_name_kanaが英字では登録できない" do
+
+      end
+      it "last_name_kanaが英字では登録できない" do
+
+      end
+      
+    end
+  end
+
 end
