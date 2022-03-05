a3c4585c81df5f1c02b4c87567d87a77d0dc9769
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-03-05 21:11:26 +0900

    Itemモデルテスト実装

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,80 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品新規登録' do
+    context '新規登録できる場合' do
+      it "全ての項目が正しければ登録できる" do
+        expect(@item).to be_valid
+      end
+    end
+
+    context "商品登録できない場合" do
+       it "nameが空では登録できない" do
+        @item.name = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+       end
+       it "infoが空では登録できない" do
+        @item.info = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Info can't be blank")
+       end
+       it "imageが空では登録できない" do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+       end
+       it "category_idが空では登録できない" do
+        @item.category_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category 選択してください")
+       end
+       it "status_idが空では登録できない" do
+        @item.status_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status 選択してください")
+       end
+       it "shipping_fee_idが空では登録できない" do
+        @item.shipping_fee_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping fee 選択してください")
+       end
+       it "prefecture_idが空では登録できない" do
+        @item.prefecture_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture 選択してください")
+       end
+       it "schedule_idが空では登録できない" do
+        @item.schedule_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Schedule 選択してください")
+       end
+       it "priceが空では登録できない" do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+       end
+       #ここまで空テスト
+       #ここから文字種類、文字数テスト
+       it "nameが40文字以上では登録できない" do
+        @item.name = "#{'a'* 41}"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name is too long (maximum is 40 characters)")
+       end
+       it "infoが1000文字以上では登録できない" do
+        @item.info = "#{'a'* 1001}"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Info is too long (maximum is 1000 characters)")
+       end
+       it "priceが半角数字以外では登録できない" do
+        @item.price = "aaaa"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+       end
+    end
+  end
 end
