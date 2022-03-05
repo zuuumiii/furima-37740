18d2bd6bcac69fba8c2884c346f3ca8c8a4544cd
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-03-05 17:05:41 +0900

    shipping_fee作成、viewに反映

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,8 +3,10 @@ class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :category
   belongs_to :status
+  belongs_to :shipping_fee
 
   validates :name, :info, :price,  presence: true
   validates :category_id, numericality: { other_than: 1, message: "選択してください" } 
   validates :status_id, numericality: { other_than: 1, message: "選択してください" } 
+  validates :shipping_fee_id, numericality: { other_than: 1, message: "選択してください" } 
 end
