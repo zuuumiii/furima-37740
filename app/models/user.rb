6b1e8cb70e28ce995941eb448711b75922066919
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-03-05 13:39:56 +0900

    メールに対するバリデーション削除（devise標準だったため）

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   VALID_PASSWORDL_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
   VALID_JAPANESE_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
   VALID_KANA_REGEX = /\A[ァ-ヶー]+\z/
   with_options presence: true do
     validates :nickname
-    validates :email, format: { with: VALID_EMAIL_REGEX, message: 'メールアドレスには@を使用してください' }
     validates :password, format: { with: VALID_PASSWORDL_REGEX, message: '英字数字両方を使用してください' }
     validates :password_confirmation, format: { with: VALID_PASSWORDL_REGEX, message: '英字数字両方を使用してください' }
     validates :first_name, format: { with: VALID_JAPANESE_REGEX, message: '全角文字を使用してください' }
