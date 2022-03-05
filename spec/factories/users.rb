09a1ea335b8d67c131751d5deb3fe6f63a2fa52a
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-03-05 15:38:25 +0900

    FactoryBotの生成内容変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              { "1a" + Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
     first_name            {"田中"}
     last_name             {"リチャード"}
