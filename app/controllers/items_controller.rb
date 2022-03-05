7bb01d1333123bc3d0bea5f7f00f2359f3bb1368
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-03-05 16:28:14 +0900

    itemモデルリレーション追加、ストロングパラメーター設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,7 +9,11 @@ class ItemsController < ApplicationController
   end
 
   def create
-
+    
   end
 
+  private
+  def message_params
+    params.require(:item).permit(:name, :image, :info, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :schedule_id, :price).merge(user_id: current_user.id)
+  end
 end
