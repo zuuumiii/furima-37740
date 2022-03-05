3da7df4a166f2fd5ff712e61dc5a9a50b56b713b
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-03-05 17:23:51 +0900

    schedule作成、viewに反映

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,10 +5,12 @@ class Item < ApplicationRecord
   belongs_to :status
   belongs_to :shipping_fee
   belongs_to :prefecture
+  belongs_to :schedule
 
   validates :name, :info, :price,  presence: true
   validates :category_id, numericality: { other_than: 1, message: "選択してください" } 
   validates :status_id, numericality: { other_than: 1, message: "選択してください" } 
   validates :shipping_fee_id, numericality: { other_than: 1, message: "選択してください" } 
   validates :prefecture_id, numericality: { other_than: 1, message: "選択してください" } 
+  validates :schedule_id, numericality: { other_than: 1, message: "選択してください" } 
 end
