a487a52004f0bed96b3d69b0562c544290a06a61
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-03-05 16:56:52 +0900

    status作成、viewに反映

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,8 +2,9 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   belongs_to :category
-
+  belongs_to :status
 
   validates :name, :info, :price,  presence: true
   validates :category_id, numericality: { other_than: 1, message: "選択してください" } 
+  validates :status_id, numericality: { other_than: 1, message: "選択してください" } 
 end
