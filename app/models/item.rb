a4808f10b6ce4cf02ef394a73c20c1324899ec82
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-03-05 16:45:35 +0900

    activehash導入、category作成、バリデーション作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  belongs_to :category
+
+
+  validates :name, :info, :price,  presence: true
+  validates :category_id, numericality: { other_than: 1, message: "選択してください" } 
 end
