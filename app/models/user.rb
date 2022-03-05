2d74471ef110275583ad36d407091ec37874236f
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-03-05 13:35:16 +0900

    user.rbリファクタリング

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_PASSWORDL_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+  VALID_JAPANESE_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
+  VALID_KANA_REGEX = /\A[ァ-ヶー]+\z/
   with_options presence: true do
     validates :nickname
-    validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: 'メールアドレスには@を使用してください' }
-    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: '英字数字両方を使用してください' }
-    validates :password_confirmation, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: '英字数字両方を使用してください' }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: '全角カタカナを使用してください' }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: '全角カタカナを使用してください' }
+    validates :email, format: { with: VALID_EMAIL_REGEX, message: 'メールアドレスには@を使用してください' }
+    validates :password, format: { with: VALID_PASSWORDL_REGEX, message: '英字数字両方を使用してください' }
+    validates :password_confirmation, format: { with: VALID_PASSWORDL_REGEX, message: '英字数字両方を使用してください' }
+    validates :first_name, format: { with: VALID_JAPANESE_REGEX, message: '全角文字を使用してください' }
+    validates :last_name, format: { with: VALID_JAPANESE_REGEX, message: '全角文字を使用してください' }
+    validates :first_name_kana, format: { with: VALID_KANA_REGEX, message: '全角カタカナを使用してください' }
+    validates :last_name_kana, format: { with: VALID_KANA_REGEX, message: '全角カタカナを使用してください' }
     validates :birth_date
   end
 
