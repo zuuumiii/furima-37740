60336503a02f7a410dee8bb5daa5b4e93629556f
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-03-05 20:33:56 +0900

    バリデーション追加、リファクタリング

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,11 +8,16 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :schedule
 
-  validates :name, :info, :image, presence: true
+  validates :name, presence: true, length: {maximum: 40}
+  validates :info, presence: true, length: {maximum: 1000}
+  validates :image, presence: true
   validates :price,  presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
-  validates :category_id, numericality: { other_than: 1, message: "選択してください" } 
-  validates :status_id, numericality: { other_than: 1, message: "選択してください" } 
-  validates :shipping_fee_id, numericality: { other_than: 1, message: "選択してください" } 
-  validates :prefecture_id, numericality: { other_than: 1, message: "選択してください" } 
-  validates :schedule_id, numericality: { other_than: 1, message: "選択してください" } 
+  with_options numericality: { other_than: 1, message: "選択してください" } do
+    validates :category_id
+    validates :status_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :schedule_id
+  end
+ 
 end
