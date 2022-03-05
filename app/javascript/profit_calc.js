509e5f09f7b5138aa7bc7eafded555ff925f28c9
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-03-05 20:12:37 +0900

    JS他ページエラー回避

--- app/javascript/profit_calc.js
+++ app/javascript/profit_calc.js
@@ -1,10 +1,12 @@
 window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
-  priceInput.addEventListener("input", () => {
-    const inputValue = priceInput.value;
-    const addTaxDom = document.getElementById("add-tax-price");
-    addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
-    const profit = document.getElementById("profit");
-    profit.innerHTML = Math.floor(inputValue * 0.9);
-  })
-});
+  if (priceInput != null) {
+    priceInput.addEventListener("input", () => {
+      const inputValue = priceInput.value;
+      const addTaxPrice = document.getElementById("add-tax-price");
+      addTaxPrice.innerHTML = Math.floor(inputValue * 0.1);
+      const profit = document.getElementById("profit");
+      profit.innerHTML = Math.floor(inputValue * 0.9);
+    })
+  }
+})
