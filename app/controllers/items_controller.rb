5dfeba887a47a1deef43ad2431530e1569d9396d
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-03-06 13:42:28 +0900

    ルーティング、アクションにeditとupdate追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,21 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    unless current_user.id == @item.user_id
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
+  end
   private
 
   def item_params
