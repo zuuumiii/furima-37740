78828982193e9bb7d05e08a800537b0008630218
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-03-07 01:24:15 +0900

    トークンと金額を取得してPayJpへ送信できる

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -13,6 +13,7 @@ class OrdersController < ApplicationController
     @order_address = OrderAddress.new(order_params)
     @item = Item.find(params[:item_id])
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -26,5 +27,13 @@ class OrdersController < ApplicationController
     params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
 end
 
