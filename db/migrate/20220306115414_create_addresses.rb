c4a01fb4515ba15fdd15eb0093451390adc11700
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-03-06 20:58:42 +0900

    addressモデル作成、カラム追加

---
+++ db/migrate/20220306115414_create_addresses.rb
@@ -0,0 +1,15 @@
+class CreateAddresses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :addresses do |t|
+      t.string :postal_code, null: false
+      t.integer :prefecture_id, null: false
+      t.string :city, null: false
+      t.string :address, null: false
+      t.string :building
+      t.string :phone, null: false
+      t.references :order, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
