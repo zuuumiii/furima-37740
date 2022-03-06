3a16897ee45c9fca7899dd9942a43c10f636918a
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-03-06 23:44:02 +0900

    バリデーション通過しなければ戻る動作を追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,11 +6,12 @@ class OrdersController < ApplicationController
   end
   def create
     @order_address = OrderAddress.new(order_params)
+    @item = Item.find(params[:item_id])
     if @order_address.valid?
       @order_address.save
       redirect_to root_path
     else
-      render :new
+      render :index
     end
   end
 
