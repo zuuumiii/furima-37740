e2f5c1b17433c96b3d46a1f82d736b3f039d66fe
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-03-06 11:09:15 +0900

    rubocop自動修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
 
   def index
-    @items = Item.all.order(created_at: "DESC")
+    @items = Item.all.order(created_at: 'DESC')
   end
 
   def new
@@ -18,9 +18,10 @@ class ItemsController < ApplicationController
     end
   end
 
-
   private
+
   def item_params
-    params.require(:item).permit(:name, :image, :info, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :schedule_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :image, :info, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :schedule_id,
+                                 :price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -8,16 +8,15 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :schedule
 
-  validates :name, presence: true, length: {maximum: 40}
-  validates :info, presence: true, length: {maximum: 1000}
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :info, presence: true, length: { maximum: 1000 }
   validates :image, presence: true
-  validates :price,  presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
-  with_options numericality: { other_than: 1, message: "選択してください" } do
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  with_options numericality: { other_than: 1, message: '選択してください' } do
     validates :category_id
     validates :status_id
     validates :shipping_fee_id
     validates :prefecture_id
     validates :schedule_id
   end
- 
 end
