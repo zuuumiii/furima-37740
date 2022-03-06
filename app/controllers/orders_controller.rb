9ec8bce4f59539da178c7b07907500c4a2f42f18
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-03-06 21:21:37 +0900

    orderのcreateアクション追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,24 @@
 class OrdersController < ApplicationController
 
   def new
-
+    @order_address = OrderAddress.new
   end
 
   def create
-
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone).merge(user_id: current_user.id, item_id: params[:id])
+  end
+
 end
+
