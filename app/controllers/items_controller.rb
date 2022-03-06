9b8f4824c4f60e5e35778bd1e12b614aa42a279e
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-03-06 13:57:31 +0900

    ビューモデル、対象のキーを記入、リンク追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,7 +24,7 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
-    unless current_user.id == @item.user_id
+    unless user_signed_in? && current_user.id == @item.user_id
       redirect_to action: :index
     end
   end
