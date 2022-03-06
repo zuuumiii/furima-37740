3892de2e92ba0e64196e7fa7fbab5d03607cf4fb
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-03-07 00:39:47 +0900

    自分の商品の購入ページへ遷移できないように追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,14 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: :index
 
   def index
     @order_address = OrderAddress.new
     @item = Item.find(params[:item_id])
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
+
   def create
     @order_address = OrderAddress.new(order_params)
     @item = Item.find(params[:item_id])
