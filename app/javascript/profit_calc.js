9ea33e6fc995cb7c04215c9412b374aab35903db
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-03-07 16:02:27 +0900

    利益計算式の端数処理を修正

--- app/javascript/profit_calc.js
+++ app/javascript/profit_calc.js
@@ -6,7 +6,7 @@ window.addEventListener('load', () => {
       const addTaxPrice = document.getElementById("add-tax-price");
       addTaxPrice.innerHTML = Math.floor(inputValue * 0.1);
       const profit = document.getElementById("profit");
-      profit.innerHTML = Math.floor(inputValue * 0.9);
+      profit.innerHTML = Math.ceil(inputValue * 0.9);
     })
   }
 })
