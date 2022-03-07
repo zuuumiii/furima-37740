4c1ae74520861127ce65fc331f62a5b0e4599db7
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-03-07 09:38:28 +0900

    sold商品の購入ページに遷移制限設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,7 +4,7 @@ class OrdersController < ApplicationController
   def index
     @order_address = OrderAddress.new
     @item = Item.find(params[:item_id])
-    if current_user.id == @item.user_id
+    if current_user.id == @item.user_id || @item.order.present?
       redirect_to root_path
     end
   end
