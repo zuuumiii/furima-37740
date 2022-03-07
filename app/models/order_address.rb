83525a040e7ec15eb023a56091089a9c051fc98c
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-03-07 21:09:39 +0900

    エラーの日本語化

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -3,15 +3,15 @@ class OrderAddress
   attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone, :user_id, :item_id, :token
 
   with_options presence: true do
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: '- を含み正しく入力してください。' }
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'はハイフンを含み正しく入力してください' }
     validates :city
     validates :address
-    validates :phone, format: { with: /\A\d{10}$|^\d{11}\z/, message: '- 無しで数字のみ入力してください。' }
+    validates :phone, format: { with: /\A\d{10}$|^\d{11}\z/, message: 'は数字のみ入力してください' }
     validates :token
     validates :user_id
     validates :item_id
   end
-  validates :prefecture_id, numericality: { other_than: 1, message: '選択してください' }
+  validates :prefecture_id, numericality: { other_than: 1, message: 'を選択してください' }
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
